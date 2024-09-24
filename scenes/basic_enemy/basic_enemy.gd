722b34f0e1576b40b594297c5f99f80ae67fb907
Charlie <35382141+charlie445@users.noreply.github.com>
2024-09-24 11:32:41 +0100

    First Commit
    First initial demo showing the few enemies, player, map and sword collision/animation

---
+++ scenes/basic_enemy/basic_enemy.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+const max_speed = 75
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Area2D.area_entered.connect(on_area_entered)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction = get_direction_to_player()
+	velocity = direction * max_speed
+	move_and_slide()
+
+
+func get_direction_to_player():
+	var player_node = get_tree().get_first_node_in_group("player") as Node2D
+	if player_node != null:
+		return (player_node.global_position - global_position).normalized()
+	return Vector2.ZERO
+
+func on_area_entered(other_area: Area2D):
+	queue_free()
+	
